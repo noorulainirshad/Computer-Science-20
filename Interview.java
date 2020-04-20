645c9adaa12f758ce7a6670e69edd28e8cdcb2b4
Noorulain Irshad <56182580+noorulainirshad@users.noreply.github.com>
2020-04-20 15:32:54 -0700

    Add files via upload

---
+++ Interview.java
@@ -0,0 +1,16 @@
+//Noorulain Irshad
+
+public class Interview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Person Interviewed:"+"\t"+"Jose Martinez");
+		System.out.println("Jose\'s Hometown:"+"\t"+"Riverdal, California");
+		System.out.print("Jose\'s Major:"+"\t"+"\t"+"Undecided"+"\n");
+		System.out.print("Jose\'s Class Standing:"+"\t"+"First Year Student"+"\n"+"This is the second CSE20 Lab");
+	}
+
+}
+/* Gave help to : Jose Martinez, explained my thought process on the code, did not show code.abstract
+* Received help from: Jose Martinez, provided me interview answers.
+* Did not collaborate with anyone*/
